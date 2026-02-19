1c9ea071ff455bf7c913676b6a53e72d58c562e8
Rishabh Sharma <rishabh.jangra04@gmail.com>
2026-02-20 00:05:53 +0530

    Fixed Vercel build errors and added env variable

--- app.py
+++ app.py
@@ -273,6 +273,7 @@ def get_hr_dashboard():
 # 6. RUN SERVER
 # ==============================
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()  # This creates your Mood and Employee tables automatically
+    app.run()
